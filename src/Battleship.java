28a0e00aa3f1d7bcb4edfe7eddce419665f50c44
SCHULLERRJ17 <SCHULLERRJ17@5CG731038Q.GCC.edu>
2018-12-01 21:05:46 -0500

    nice2.0

--- src/Battleship.java
+++ src/Battleship.java
@@ -37,12 +37,6 @@ public class Battleship extends Application {
 		aiGrid = new Grid();
 		
 		aiGrid.randomSet();
-		for(int i = 0; i < 10; i++) {
-			for(int x = 0; x < 10; x++) {
-				System.out.print(!aiGrid.isEmpty(i, x));
-			}
-			System.out.println();
-		}
 		
 		primaryStage = ps;
 		GridPane g1 = new GridPane();
